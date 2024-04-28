fcd7f0cbc062c617004edb6da67b3bfda2436d84
sarvagya-code <sarvagyasrivastava8@gmail.com>
2024-04-28 16:24:24 +0530

    XDisplayName completed

--- xdisplayname/src/App.js
+++ xdisplayname/src/App.js
@@ -19,17 +19,20 @@ function App() {
           <h1>Full Name Display</h1>
         </div>
           <form onSubmit={handleSubmit}>
-            <div>
-              <p>First Name:</p>
-              <input type='text' value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
+            <div style={{display : "flex", flexDirection: "column"}}>
+            <div style={{display : "flex", alignItems:"center"}}>
+              <label>First Name:</label>
+              <input type='text' value={firstName} onChange={(e)=>setFirstName(e.target.value)} style={{height : "1rem"}}/>
             </div>
-            <div>
-              <p>Last Name:</p>
-              <input type='text' value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+            
+            <div style={{display : "flex",alignItems:"center"}}>
+              <label>Last Name:</label>
+              <input type='text' value={lastName} onChange={(e)=>setLastName(e.target.value)} style={{height : "1rem"}}/>
             </div>
-            <button type='submit'>Submit</button>
+            <button type='submit' style={{width:"4rem"}}>Submit</button>
+            </div>
           </form>
-          {submit ? (<p>Full Name: {firstName} {lastName}</p>) : <p></p>}
+          {firstName && lastName && submit ? (<p>Full Name: {firstName} {lastName}</p>) : <p></p>}
     </div>
   );
 }
